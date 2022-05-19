49df0fe5d3932d210ad7eb061c942b76f9fc245c
Renu kumari <renuk1485@gmail.com>
2022-05-19 07:03:46 +0530

    commit

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,8 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://localhost:27017/try")
+  .connect(
+    "mongodb+srv://renurisha:Renu8271%40123@cluster0.i8evq.mongodb.net/?retryWrites=true&w=majority"
+  )
   .then(() => console.log("connection success..."))
   .catch((e) => {
     console.log(e);
